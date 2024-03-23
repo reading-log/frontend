3862d8a6b389a6ec063373f11c1e2624b3dd9980
nemo-nemo-bim <drakequation@naver.com>
2024-03-24 03:14:41 +0900

    #4 [feat] 회원가입 기능 구현

--- src/apis/userApi.ts
+++ src/apis/userApi.ts
@@ -5,3 +5,13 @@ export const onLogin = async (data: { email: string; password: string }) => {
   const response = await axios.post('api/members/login', data)
   return response
 }
+
+/**회원가입 */
+export const onJoin = async (data: FormData) => {
+  const response = await axios.post('api/members/join', data, {
+    headers: {
+      'Content-Type': 'multipart/form-data',
+    },
+  })
+  return response
+}
