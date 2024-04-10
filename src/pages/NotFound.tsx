d818be91613478a20fee1ce17856adb8536a2b36
nemo-nemo-bim <drakequation@naver.com>
2024-04-10 20:06:43 +0900

    #23 [feat] 404페이지 스타일링

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,28 @@
+import { css } from '@emotion/react'
+import { faTriangleExclamation } from '@fortawesome/free-solid-svg-icons/faTriangleExclamation'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { DefaultLayout } from '../components/Layouts'
+import { calcRem, colors } from '../styles/theme'
 
 const NotFound = () => {
-  return <DefaultLayout>404 Not Found</DefaultLayout>
+  return (
+    <DefaultLayout>
+      <div css={notFound}>
+        <FontAwesomeIcon icon={faTriangleExclamation} size="8x" color={colors.main1} />
+        <p>페이지를 찾을 수 없습니다.</p>
+      </div>
+    </DefaultLayout>
+  )
 }
 
 export default NotFound
+
+const notFound = css`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  p {
+    margin-top: ${calcRem(27)};
+  }
+`
